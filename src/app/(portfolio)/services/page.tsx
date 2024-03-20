304b2fb993ee95cac169e8a4c0d798749d02e9a9
noah milliken <noahmilliken@gmail.com>
2024-03-20 18:36:43 -0400

    remove tech icons

--- src/app/(portfolio)/services/page.tsx
+++ src/app/(portfolio)/services/page.tsx
@@ -1,19 +1,15 @@
 import MenuCards from "@/components/MenuCards";
-import { icons } from "./techIcons";
-import TechImageIcon from "@/components/TechImageIcon";
 
 function page() {
-  const iconsArr = icons;
   return (
     <div className="flex flex-col justify-center w-full">
       <div className="flex flex-col justify-center mx-auto ">
         <MenuCards />
-        <h1 className="mx-auto">Technologies Used</h1>
+        {/* <h1 className="mx-auto">Technologies Used</h1>
         <div className="grid grid-cols-4 gap-4 m-4 sm:m-8">
           {iconsArr.map((icon, idx) => (
             <TechImageIcon key={icon} src={icon} dimension={60} />
-          ))}
-        </div>
+          ))} */}
       </div>
     </div>
   );
